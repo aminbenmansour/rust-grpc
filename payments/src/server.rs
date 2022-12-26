7e3e05fda9ffcddd8577b2ebea1fd690a72a78c1
Amine Ben Mansour <amin.benmansour.10@gmail.com>
2022-12-26 23:27:01 +0100

    importing types tonic generated to us based on protobuffs definitions

--- payments/src/server.rs
+++ payments/src/server.rs
@@ -1,1 +1,5 @@
+use tonic::{transport::Server, Request, Response, Status};
+
+use payments::bitcoin_server::{Bitcoin, BitcoinServer};
+use payments::{BtcPaymentResponse, BtcPaymentRequest}
 fn main() {}
