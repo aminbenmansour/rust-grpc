31998d66eccdcc7bd177d1c0b10378c35258e30c
Amine Ben Mansour <amin.benmansour.10@gmail.com>
2022-12-26 23:55:30 +0100

    implementing an asyncronous bitcoin trait for BitcoinService struct

--- payments/src/server.rs
+++ payments/src/server.rs
@@ -10,5 +10,25 @@ pub mod payments {
 #[derive(Debug, Default)]
 pub struct BitcoinService {}
 
+#[tonic::async_trait]
+impl Bitcoin for BitcoinService {
+    async fn send_payment(
+        &self,
+        request: Request<BtcPaymentRequest>,
+    ) -> Result<Response<BtcPaymentResponse>, Status> {
+        println!("Got a request: {:?}", request);
+
+        let req = request.into_inner();
+
+        let reply = BtcPaymentResponse {
+            successful: true, // always true as this is a mocking example
+                              // Does not actually connect to the blockchain 
+                              // and commits transactions 
+            message: format!("Sent {}BTC to {}.", req.amount, req.to_addr).into(),
+        };
+
+        Ok(Response::new(reply))
+    }
+}
 
 fn main() {}
