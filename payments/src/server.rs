871129162f0f2235fea1727315d355e4b90bcdcf
Amine Ben Mansour <amin.benmansour.10@gmail.com>
2022-12-27 01:15:56 +0100

    instantiating service and defining server's address

--- payments/src/server.rs
+++ payments/src/server.rs
@@ -33,5 +33,8 @@ impl Bitcoin for BitcoinService {
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let addr = "[::1]:50051".parse()?;
+    let btc_service = BitcoinService::default();
+    
     Ok(())
 }
